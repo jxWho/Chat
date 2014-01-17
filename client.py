aae85bb8e561f732f885b7b831eb1a3057d12bbc
= <hjxwho@gmail.com>
2014-01-17 00:23:49 -0500

    Class redesigned Make a generic server class

--- client.py
+++ client.py
@@ -1,4 +1,5 @@
 import socket
+import thread
 
 ADD = '127.0.0.1'
 PORT = 4000
@@ -7,15 +8,31 @@ ADDR = (ADD, PORT)
 RECEIVE_ADDR = (ADD, RECEIVE_PORT)
 class chatClient:
     def __init__(self):
-        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        pass
 
     def _runCode(self):
+        #start a thread to talk to the server
+        thread.start_new_thread(_createSocketToTalkToServer,())
+        #start a thread to receive message from other clients
+        thread.start_new_thread(_createSocketAsServer,())
+
+
+    def start(self):
+        self._runCode()
+
+    #used to talk to the server
+    def _createSocketToTalkToServer(self):
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.s.connect(ADDR)
         while True:
             data = self.s.recv(1024)
             if not data:
                 print 'No response from Server'
                 break
+            elif str(data) == '000':
+                #return my socket address
+                global RECEIVE_PORT
+                self.s.send( RECEIVE_PORT )
             else:
                 print str(data)
 
@@ -29,10 +46,8 @@ class chatClient:
             print str(data)
         self.s.close()
 
-    def start(self):
-        self._runCode()
-
-    def _createSocket(self):
+    # used to be a server and receive message from other clients
+    def _createSocketAsServer(self):
         self.receiveSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.receiveSocket.setsockopt(socket.SOL_SOCKET,
                                     socket.SO_REUSEADDR,1)
@@ -50,3 +65,5 @@ class chatClient:
 
         self.receiveSocket.listen(5)
 
+        while True:
+            
